7112508845e4544b378c18e6629a344309c60c92
Thomas Wilhelm <thomas.p.wilhelm@gmail.com>
2015-05-19 12:08:30 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Laravel Countries
+
+This package provides ease access to [umpirsky/country-list](https://github.com/umpirsky/country-list) country arrays.
---
+++ src/ServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Quodos\LaravelCountries;
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+class ServiceProvider extends LaravelServiceProvider {
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot() {
+
+        // handling things... :)
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register() {
+
+        // Bind any implementations.
+
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides() {
+
+        return [];
+    }
+}
